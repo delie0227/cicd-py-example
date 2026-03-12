1286d4c7cb8496736edbc3a626e7d0825a4d16af
delie0227 <delie0227@gmail.com>
2026-03-12 19:26:48 +0100

    megint elrontott teszt

--- test_mymath.py
+++ test_mymath.py
@@ -6,7 +6,7 @@ def test_add_positive():
     assert add_numbers(1, 2) == 3
 
 def test_add_zero():
-    assert add_numbers(1, 0) == 1
+    assert add_numbers(1, 0) == 0
 
 def test_add_negative():
     assert add_numbers(4, -100) == -96
@@ -14,6 +14,7 @@ def test_add_negative():
 def test_add_string__expect_exception():
     with pytest.raises(TypeError):
         add_numbers(4, 'I DO NOT BELONG HERE')
+
 
 
 
